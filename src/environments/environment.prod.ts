a3fe3168e842f3c200fa3be5474d1c1cdd7ac739
nkokkiligadda87 <nkokkiligadda87@gmail.com>
2024-08-02 16:14:53 +0530

    CSS Changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -21,9 +21,9 @@
 */
 
 
-const ecdIP = 'http://amritwprdev.piramalswasthya.org:8080/';
-const commonIP = 'http://amritwprdev.piramalswasthya.org:8080/';
-const adminIP = 'http://amritwprdev.piramalswasthya.org:8080/';
+const ecdIP = 'https://amritwprdev.piramalswasthya.org/';
+const commonIP = 'https://amritwprdev.piramalswasthya.org/';
+const adminIP = 'https://amritwprdev.piramalswasthya.org/';
 const COMMON_API = `${commonIP}commonapi-ecd/`;
 const ADMIN_API = `${adminIP}adminapi-v1.0/`;
 const ECD_API = `${ecdIP}ecdapi-v1.0/`;
