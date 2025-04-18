9a22724b08257475eb957b65c2a62de4b7fd4783
srinuatpiramal <srinivas.tirunahari@piramalswasthya.org>
2025-04-18 13:58:57 +0530

    Update environment.test.ts

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -32,9 +32,6 @@ const sessionStorageEncKey = '';
 const fhirIP = 'http://amritwprdev.piramalswasthya.org:8080/';
 const FHIR_API = `${fhirIP}fhirapi-v1.0/`;
 
-const fhirIP = 'http://amritwprdev.piramalswasthya.org:8080/';
-const FHIR_API = `${fhirIP}:8085/`;
-
 export const environment = {
   production: true,
   encKey: sessionStorageEncKey,
